107103c9e46bb714879b7c039df40d59e662f376
kaihel <895421405@qq.com>
2021-05-28 17:00:25 +0800

    输出孙玮策

--- src/main/java/com/sr/controller/UserController.java
+++ src/main/java/com/sr/controller/UserController.java
@@ -13,6 +13,6 @@ public class UserController {
         System.out.println("1");
         System.out.println("222");
         System.out.println("3333");
-        return "hallWord";
+        return "sunwece";
     }
 }
